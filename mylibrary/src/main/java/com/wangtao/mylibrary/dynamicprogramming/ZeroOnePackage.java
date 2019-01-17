416a127ec21ce0208c43eac8dc72403c69c50232
wangtao <1473095453@qq.com>
2019-01-17 15:53:20 +0800

    动态规划-简单0/1背包问题,使用一维数组优化

--- mylibrary/src/main/java/com/wangtao/mylibrary/dynamicprogramming/ZeroOnePackage.java
+++ mylibrary/src/main/java/com/wangtao/mylibrary/dynamicprogramming/ZeroOnePackage.java
@@ -21,7 +21,7 @@ public class ZeroOnePackage {
         for (int i = 1; i < n; i++) {
             //第i个物品不加入背包
             for (int j = 0; j < w; j++) {
-                if (states[i - 1][j] == true) states[i][j] = true;
+                if (states[i - 1][j]) states[i][j] = true;
             }
             //第i个物品加入背包
             for (int j = 0; j <= w - weight[i]; j++) {
@@ -32,6 +32,36 @@ public class ZeroOnePackage {
         for (int i = w; i >= 0; i--) {
             if (states[n - 1][i]) return i;
         }
+        return 0;
+    }
+
+    /**
+     * 背包问题，优化，只使用一维数组
+     *
+     * @param weight
+     * @param n
+     * @param w
+     * @return
+     */
+    public static int knapsack2(int[] weight, int n, int w) {
+        boolean[] states = new boolean[w + 1];
+        //第一个物品特殊处理，哨兵
+        states[0] = true;
+        states[weight[0]] = true;
+
+        for (int i = 1; i < n; i++) {
+            //第i个物品加入背包，从大到小，避免重复
+            for (int j = w - weight[i]; j >= 0; j--) {
+                if (states[j]) {
+                    states[j + weight[i]] = true;
+                }
+            }
+        }
+
+        for (int i = w; i >= 0; i--) {
+            if (states[i]) return i;
+        }
+
         return 0;
     }
 }
