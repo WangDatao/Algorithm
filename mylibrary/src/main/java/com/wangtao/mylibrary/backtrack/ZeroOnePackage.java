9ab7016b875c825a862d30b4d0ee74b71e92acc7
wangtao <1473095453@qq.com>
2019-01-17 10:45:39 +0800

    回溯算法-0/1背包问题优化

--- mylibrary/src/main/java/com/wangtao/mylibrary/backtrack/ZeroOnePackage.java
+++ mylibrary/src/main/java/com/wangtao/mylibrary/backtrack/ZeroOnePackage.java
@@ -8,28 +8,68 @@ package com.wangtao.mylibrary.backtrack;
  * @date 2019/1/16 14:58
  */
 public class ZeroOnePackage {
-    public int maxW = Integer.MIN_VALUE; // 存储背包中物品总重量的最大值
+    /**
+     * 存储背包中物品总重量的最大值
+     */
+    public int maxW = Integer.MIN_VALUE;
+    /**
+     * 物品重量数组
+     */
+    private int[] items;
+    /**
+     * 物品个数 items.length
+     */
+    private int n;
+    /**
+     * 背包可承受的重量
+     */
+    private int w;
+    /**
+     * 优化，记录已经出现过的情况
+     */
+    public boolean[][] memory;
 
-    // 假设背包可承受重量 100，物品个数 10，物品重量存储在数组 a 中，那可以这样调用函数：
-    // f(0, 0, a, 10, 100)
+    public ZeroOnePackage(int w, int[] items) {
+        this.n = items.length;
+        this.w = w;
+        this.items = items;
+        memory = new boolean[n][w];
+    }
+
 
     /**
-     * @param i     考察到的物品下标
-     * @param cw    背包中已有的重量
-     * @param items 物品重量数组
-     * @param n     物品个数 items.length
-     * @param w     背包可承受的重量
+     * @param i  考察到的物品下标
+     * @param cw 背包中已有的重量
      */
-    public void f(int i, int cw, int[] items, int n, int w) {
+    public void f(int i, int cw) {
         if (cw == w || i == n) { // cw==w 表示装满了 ;i==n 表示已经考察完所有的物品
             if (cw > maxW) maxW = cw;
             return;
         }
         //表示不把第i个物品放入背包
-        f(i + 1, cw, items, n, w);
+        f(i + 1, cw);
         //表示把第i个物品放入背包
         if (cw + items[i] <= w) {// 已经超过可以背包承受的重量的时候，就不要再装了
-            f(i + 1, cw + items[i], items, n, w);
+            f(i + 1, cw + items[i]);
+        }
+    }
+
+    /**
+     * 利用memory 优化
+     * @param i
+     * @param cw
+     */
+    public void ff(int i, int cw) {
+        if (cw == w || i == n) {
+            maxW = cw > maxW ? cw : maxW;
+            return;
+        }
+        if (memory[i][cw])
+            return;
+        memory[i][cw] = true;
+        f(i + 1, cw);
+        if (cw + items[i] <= w) {
+            f(i + 1, cw + items[i]);
         }
     }
 
