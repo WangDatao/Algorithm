c2a64edf1fbe5b7e83772d49db74b9a662debf78
wangtao <1473095453@qq.com>
2019-08-12 11:22:07 +0800

    算法-合并两个有序链表-o17

--- mylibrary/src/main/java/com/wangtao/mylibrary/algorithm/linkedlist/MergeTwoOrderLinkedList.java
+++ mylibrary/src/main/java/com/wangtao/mylibrary/algorithm/linkedlist/MergeTwoOrderLinkedList.java
@@ -1,49 +1,57 @@
 package com.wangtao.mylibrary.algorithm.linkedlist;
 
-import com.wangtao.mylibrary.datastructure.linkedlist.Node;
+import com.wangtao.mylibrary.datastructure.linkedlist.ListNode;
 
 /**
  * @author wangtao
- * @Description: 合并两个有序链表
+ * @Description: offer(17):合并两个有序链表
  * @date 2019/5/13 14:41
  */
+
+/**
+ * 链表的基本操作；
+ * 鲁棒性
+ */
 public class MergeTwoOrderLinkedList {
+    public static void main(String[] args) {
+        ListNode l1 = ListNode.createLinkedList(3);
+        ListNode l2 = ListNode.createLinkedList(5);
 
-    public static Node<Integer> mergeTwoList(Node<Integer> l1, Node<Integer> l2) {
-        Node<Integer> head = null;
-        Node<Integer> tail = null;
+        MergeTwoOrderLinkedList merger = new MergeTwoOrderLinkedList();
+        ListNode node = merger.Merge(l1, l2);
+        ListNode.printList(node);
 
-        if (null == l1) return l2;
-        if (null == l2) return l1;
+    }
 
-        while (l1 != null && l2 != null) {
-            if (l1.value < l2.value) {
-                if (head == null) {
-                    head = tail = l1;
-                    l1 = l1.next;
-                } else {
-                    tail.next = l1;
-                    tail = l1;
-                    l1 = l1.next;
-                }
+    public ListNode Merge(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null) return null;
+
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        ListNode head = null;
+        ListNode tail = null;
+
+        while (list1 != null && list2 != null) {
+            ListNode next = null;
+            //找到两个链表中较小值节点
+            if (list1.val < list2.val) {
+                next = list1;
+                list1 = list1.next;
             } else {
-                if (head == null) {
-                    head = tail = l2;
-                    l2 = l2.next;
-                } else {
-                    tail.next = l2;
-                    tail = l2;
-                    l2 = l2.next;
-                }
+                next = list2;
+                list2 = list2.next;
             }
-
+            //插入链表
+            if (head == null) {//头结点
+                tail = head = next;
+            } else {
+                tail.next = next;
+                tail = tail.next;
+            }
         }
-
-        if (null != l1) {
-            tail.next = l1;
-        } else if (null != l2) {
-            tail.next = l2;
-        }
+        //剩余的部分
+        tail.next = list1 == null ? list2 : list1;
 
         return head;
     }
